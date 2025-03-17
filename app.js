2e04c8fd0c70e2e0a7fb8a1035ca3d3e4aff157a
armandojosefloresvega <armandojosefloresvega@gmail.com>
2025-03-17 18:56:56 -0500

    función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 let amigos = []
+
+const agregarAmigo = () => {
+  const input = document.getElementById("amigo");
+  const lista = document.getElementById("listaAmigos");
+
+  const nombre = input.value.trim();
+  if (nombre === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  amigos.push(nombre);
+
+  const nuevoItem = document.createElement("li");
+  nuevoItem.textContent = nombre;
+  lista.appendChild(nuevoItem);
+
+  input.value = "";
+  input.focus();
+}
